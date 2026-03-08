d0fcaea73d27be516f61a3b523cf31637a200f70
MP-XXL <royceapera@yahoo.com>
2026-03-08 05:15:33 -0500

    Adds more payload data for login

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -21,9 +21,10 @@ def register(user_data: UserCreate, db: Session = Depends(get_db)):
 def login(login_data: UserLogin, db: Session = Depends(get_db)):
     auth_service = AuthService(db)
     result = auth_service.login(login_data)
-    return {
-        "access_token": result["access_token"],
-        "token_typess": result["token_type"],
-        "user": result["user"],
-        "admin_status": result["admin_status"]
-    }
+    return result
+    # return {
+    #     "access_token": result["access_token"],
+    #     "token_type": result["token_type"],
+    #     "user": result["user"],
+    #     "admin_status": result["admin_status"]
+    # }
